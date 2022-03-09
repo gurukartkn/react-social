ad604d9173694bba593c0787ac67236657023df5
Guru Karthikeyan <gurukartkn@gmail.com>
2022-03-09 13:32:49 +0530

    toggle sun

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "./themeContext";
-import { BsCloudSun, BsCloudMoonFill } from "react-icons/bs";
+import { BsCloudSunFill, BsCloudMoonFill } from "react-icons/bs";
 
 export const Toggle = () => {
   const { theme, setTheme } = useContext(ThemeContext);
@@ -24,7 +24,7 @@ export const Toggle = () => {
       {theme === "dark" ? (
         <BsCloudMoonFill className="w-6 h-6 -translate-y-3.5 text-slate-100 shadow-lg" />
       ) : (
-        <BsCloudSun className="w-6 h-6 -translate-y-3.5 text-amber-400 shadow-lg" />
+        <BsCloudSunFill className="w-6 h-6 -translate-y-3.5 text-amber-400 shadow-lg" />
       )}
     </label>
   );
